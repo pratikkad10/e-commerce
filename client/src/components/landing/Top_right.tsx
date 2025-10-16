440747d430ba165febe0fdc86a3a8d6820666533
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-16 22:17:08 +0530

    updated

--- client/src/components/landing/Top_right.tsx
+++ client/src/components/landing/Top_right.tsx
@@ -1,7 +1,7 @@
 
 const Top_right = () => {
     return (
-        <div className="section-top-right trending-image bg-card-foreground mt-30 rounded-3xl overflow-hidden ">
+        <div className="section-top-right trending-image bg-card-foreground mt-4 lg:mt-0  rounded-3xl overflow-hidden ">
             <img src="/men-trending.png" alt="Trending" loading="lazy" className="h-100 w-sm object-fill" />
         </div>
     )
