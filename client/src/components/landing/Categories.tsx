9e99f913974acd7ec968ff6b89f17b530465bcc5
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-16 22:16:10 +0530

    categories updated

--- client/src/components/landing/Categories.tsx
+++ client/src/components/landing/Categories.tsx
@@ -7,8 +7,8 @@ const Categories = () => {
         { name: "Kid's", image: "/kids-studio.jpg", path: "/category/kids" },
     ]
     return (
-        <div className="">
-            <h2 className="lg:text-4xl text-foreground/90 text-2xl text-center font-semibold  ">Our categories</h2>
+        <div className="lg:mx-40">
+            <h2 className="lg:text-4xl text-foreground/90 text-2xl text-center font-semibold ">Our categories</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 mt-6 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-40 bg-background">
                 {categories.map((category) => (
                     <CategoryCard
