64c7e8061683d6eb93b7a95e5b1726c98b49d645
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-16 22:17:37 +0530

    updated

--- client/src/components/landing/Wrapper2.tsx
+++ client/src/components/landing/Wrapper2.tsx
@@ -3,7 +3,7 @@ import { brands } from "@/utils/brands"
 
 const Wrapper2 = () => {
   return (
-    <div className="brands card mt-10 mb-10 lg:mx-40 py-6 rounded-lg text-center">
+    <div className="brands card mb-8 lg:mx-40 py-6 rounded-lg text-center">
       <h2 className="lg:text-4xl text-foreground/90 text-2xl text-center font-semibold py-4">Our brands</h2>
       <div className="brand-logos flex flex-wrap lg:space-x-[1.6rem] sm:px-6 md:px-12   xl:px-40 justify-center gap-4">
         {brands.map((brand, idx) => (
