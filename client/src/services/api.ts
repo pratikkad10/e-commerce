2954cd4a293bc00d6af0edcba1bf7089e1e80116
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-16 17:41:20 +0530

    api implemented

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://e-commerce-ghmy.onrender.com/api/v1",
+    baseURL: "http://localhost:5000/api/v1",
     withCredentials: true,
 });
 
