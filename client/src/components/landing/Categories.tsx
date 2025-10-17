52096c3731f794466fb8e289f3a1bac91bdf4f7f
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-17 21:16:57 +0530

    design updated

--- client/src/components/landing/Categories.tsx
+++ client/src/components/landing/Categories.tsx
@@ -9,7 +9,7 @@ const Categories = () => {
     return (
         <div className="lg:mx-40">
             <h2 className="lg:text-4xl text-foreground/90 text-2xl text-center font-semibold ">Our categories</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 mt-6 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-40 bg-background">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 mt-6 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-40">
                 {categories.map((category) => (
                     <CategoryCard
                         key={category.name}
